61151bcd490ef8b63c79a5a559bc33648b6203df
MarlonProgram <Mgabrielur@gmail.com>
2024-04-10 04:22:44 -0400

    payment-client layout done!

--- src/app/payment-client/payment-client.component.ts
+++ src/app/payment-client/payment-client.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
-  selector: 'app-payment-client',
+  selector: 'payment-client',
   standalone: true,
-  imports: [],
+  imports: [InputTextModule, ButtonModule],
   templateUrl: './payment-client.component.html',
-  styleUrl: './payment-client.component.scss'
+  styleUrl: './payment-client.component.scss',
 })
-export class PaymentClientComponent {
-
-}
+export class PaymentClientComponent {}
