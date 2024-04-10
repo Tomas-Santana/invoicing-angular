cd85f80184b561ff6e798875dfd8a5ef64094b6c
MarlonProgram <Mgabrielur@gmail.com>
2024-04-10 03:36:49 -0400

    Modal de payment-view terminado (visualmente)

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-import { InvoicingSearchComponent } from '../invoicing-search/invoicing-search.component' 
+import { InvoicingSearchComponent } from '../invoicing-search/invoicing-search.component';
 import { InvoicingViewComponent } from '../invoicing-view/invoicing-view.component';
+import { InvoicingDialogComponent } from '../invoicing-dialog/invoicing-dialog.component';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [InvoicingSearchComponent, InvoicingViewComponent],
+  imports: [
+    InvoicingSearchComponent,
+    InvoicingViewComponent,
+    InvoicingDialogComponent,
+  ],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.scss'
+  styleUrl: './search.component.scss',
 })
-export class SearchComponent {
-  
-}
+export class SearchComponent {}
