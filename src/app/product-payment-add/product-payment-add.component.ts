d97bf3ee0c6345cc0de8ece2cd6413b6dc885480
Rafael Mata <matarafael2004@gmail.com>
2024-04-10 20:54:49 -0400

    Co-authored-by: MarlonUrda <MarlonUrda@users.noreply.github.com> Co-authored-by: Andres Garcia <Atla004@users.noreply.github.com>

--- src/app/product-payment-add/product-payment-add.component.ts
+++ src/app/product-payment-add/product-payment-add.component.ts
@@ -2,14 +2,21 @@ import { Component } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { ButtonModule } from 'primeng/button';
+import { SearchModalComponent } from '../search-modal/search-modal.component';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
 
 @Component({
   selector: 'product-payment-add',
   standalone: true,
-  imports: [InputTextModule, InputNumberModule, ButtonModule],
+  imports: [InputTextModule, InputNumberModule, ButtonModule, SearchModalComponent, OverlayPanelModule],
   templateUrl: './product-payment-add.component.html',
   styleUrl: './product-payment-add.component.scss'
 })
 export class ProductPaymentAddComponent {
 
+  currentQuantity:number = 0
+  setQuantity(amount: number) {
+    if (this.currentQuantity + amount < 0) return;
+    this.currentQuantity += amount;
+  }
 }
--- src/app/search-modal/search-modal.component.ts
+++ src/app/search-modal/search-modal.component.ts
@@ -3,6 +3,7 @@ import { InputTextModule } from 'primeng/inputtext';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { OverlayModule } from 'primeng/overlay';
+import { HttpClientModule, HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'search-modal',
@@ -11,4 +12,6 @@ import { OverlayModule } from 'primeng/overlay';
   templateUrl: './search-modal.component.html',
   styleUrl: './search-modal.component.scss',
 })
-export class SearchModalComponent {}
+export class SearchModalComponent {
+  
+}
